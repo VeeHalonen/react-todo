798b0af77074fb01f430d63c139257bfdd7b461a
VeeHalonen <60449132+VeeHalonen@users.noreply.github.com>
2020-06-29 08:33:00 +0300

    Attempt to fix server-side rendering

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import theme from "../src/theme";
+import theme from "../theme";
 
 // https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
 
